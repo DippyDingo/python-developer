727a9d26c58715fa2ae109e3fd2962456c4adcf9
Alexey Melnikov <warmfarm109@gmail.com>
2025-07-31 18:39:15 +0300

    Удалил токен в ДЗ "Переводчик"

--- 3.ООП_и_работа_с_API/4.Работа_с_библиотекой_requests_http_запросы/2.translate.py
+++ 3.ООП_и_работа_с_API/4.Работа_с_библиотекой_requests_http_запросы/2.translate.py
@@ -3,7 +3,7 @@
 import requests
 
 url = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
-token = 'dict.1.1.20220928T183617Z.4449b33063fe4328.b93679d48620ed6f3c20da6bff0237bcbd0e8d6a'
+token = ''
 
 def translate_word(word):
     """
